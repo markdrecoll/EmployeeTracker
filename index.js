8447edf5032f3e6fa977fd494fba9db3ec03cd8b
mdre797 <markdrecoll@gmail.com>
2021-05-05 15:18:29 -0500

    Worked on getting employees passed in for manager assignment.

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    host:'localhost',
+    port: 3306,
+    user:'root',
+    password: '19cavalryarcher',
+    database:'employeetracker_db'
+})
+
 // the options for adding another employee or finish adding
 const whatWouldUserLikeToDoArray = [
     {
@@ -18,12 +26,80 @@ const whatWouldUserLikeToDoArray = [
         name: 'whatWouldUserLikeToDo'
     }
 ]
+const availableEmployeesOnLoad = getAllEmployees();
+console.log(availableEmployeesOnLoad);
+const addEmployeeQuestions = [
+    {
+        type: 'input',
+        message: "What is the employee's first name?",
+        name: 'employeeFirstNameInput'
+    },
+    {
+        type: 'input',
+        message: "What is the employee's last name?",
+        name: 'employeeLastNameInput'
+    },
+    {
+        type: 'list',
+        message: "What is the employee's role?",
+        choices: ['Sales Lead', 'Salesperson', 'Lead Engineer', 'Software Engineer', 'Accountant', 'Legal Team Lead', 'Attorney'],
+        name: 'employeeRoleInput'
+    },
+    {
+        type: 'list',
+        message: "Who is the employee's manager?",
+        // choices: ['fdaf', 'fasdf'],
+        choices: availableEmployeesOnLoad,
+        name: 'employeeManagerInput'
+    }
+]
+
+async function getAllEmployees() {
+    try {
+        const employees = await connection.query('SELECT * FROM employee', (err, res) => {
+            if (err) throw err;
+            // res.data? is all the employees;
+            const employees = res.map(employee => {
+                return `${employee.id}: ${employee.first_name} ${employee.last_name}`
+            });
+            return employees;
+        })
+        if (!employees) {
+            console.log('no employees error')
+        }
+        return employees;
+    } catch (err) {
+        throw err;
+    }
+}
+
+function showAllEmployees() {
+    const employees = getAllEmployees();
+    console.log(employees);
+}
+
+function addEmployee(){
+    inquirer
+        .prompt(addEmployeeQuestions)
+        .then((response) =>{
+
+            console.log(response.employeeFirstNameInput + " " + response.employeeLastNameInput)
+        }).catch(err => {
+            console.log(err);
+
+        
+        })
+}
 
 function userInteractionPrompt() {
     inquirer
         .prompt(whatWouldUserLikeToDoArray)
         .then((response) => {
-            if(response.whatWouldUserLikeToDo !== 'Exit'){
+            if(response.whatWouldUserLikeToDo === 'Add Employee'){
+                addEmployee();
+            }else if(response.whatWouldUserLikeToDo === 'View All Employees'){
+                showAllEmployees();            
+            }else if(response.whatWouldUserLikeToDo !== 'Exit'){
                 console.log("User choice selected.");
                 userInteractionPrompt();
             }
--- seeds.sql
+++ seeds.sql
@@ -3,14 +3,18 @@ INSERT INTO department (name) VALUES ('Engineering');
 INSERT INTO department (name) VALUES ('Finance');
 INSERT INTO department (name) VALUES ('Legal');
 
-INSERT INTO role (title, salary, department_id) VALUES ('Sales Lead', '100000', 1);
-INSERT INTO role (title, salary, department_id) VALUES ('Salesperson', '80000', 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Sales Lead', '95000', 1);
+INSERT INTO role (title, salary, department_id) VALUES ('Salesperson', '85000', 1);
 INSERT INTO role (title, salary, department_id) VALUES ('Lead Engineer', '145000', 2);
 INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', '110000', 2);
 INSERT INTO role (title, salary, department_id) VALUES ('Accountant', '115000', 3);
 INSERT INTO role (title, salary, department_id) VALUES ('Legal Team Lead', '240000', 4);
 INSERT INTO role (title, salary, department_id) VALUES ('Attorney', '170000', 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('John', 'Smith', 1, null);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bob', 'Johnson', 2, 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Joe', 'Jackson', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Arnold', 'Schwartzenegger', 1, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Sylvester', 'Stallone', 2, 1);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jean-Claude', 'VanDamme', 3, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Bruce', 'Willis', 4, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Steven', 'Seagal', 5, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Mel', 'Gibson', 6, null);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jason', 'Statham', 7, 6);
