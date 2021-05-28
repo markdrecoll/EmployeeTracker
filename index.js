3aac58d9be5ca92ce75cd75b97508e1563d0e19c
Mark Drecoll <77694281+markdrecoll@users.noreply.github.com>
2021-05-27 20:55:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '19cavalryarcher',
+    password: 'root',
     database: 'employeetracker_db'
 })
 
